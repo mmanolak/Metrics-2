4ff4342239d2162a4a21e24600d04ad17965a6d6
M. Manolakis <mmanolak@hawaii.edu>
2025-12-05 12:14:22 -1000

    created the clean and compiled for a better appeal

---
+++ Clean PS5.R
@@ -0,0 +1,48 @@
+#Michael Manolakis 
+#Econ 629 Econometrics 2 - CRN 79650
+#Problem Set 5 - Clean Version (Minimal Comments)
+#5 December 2025
+
+library(tidyverse)
+#library(wooldridge)
+
+# Defining Parameters
+n_sims <- 50
+n <- 500
+
+# Container for Results
+sim_results <- tibble(iteration = 1:n_sims, beta_hat_x1 = NA_real_, beta_hat_x2 = NA_real_, beta_hat_x3 = NA_real_)
+
+for (i in 1:n_sims) {
+  # DGP
+  x <- rnorm(n = n, mean = 1, sd = 1)
+  v <- rnorm(n = n, mean = 0, sd = 1)
+  y <- 1 + 0.5 * x + v
+  
+  e1 <- rnorm(n = n, mean = 0, sd = sqrt(0.25))
+  e2 <- rnorm(n = n, mean = 0, sd = sqrt(0.5))
+  e3 <- rnorm(n = n, mean = 0, sd = sqrt(19))
+  
+  x1 <- x + e1
+  x2 <- x + e2
+  x3 <- x + e3
+  
+  df_iter <- tibble(y, x1, x2, x3)
+  
+  # Estimation and Storage
+  ols_model1 <- lm(y ~ x1, data = df_iter)
+  sim_results$beta_hat_x1[i] <- coef(ols_model1)[["x1"]]
+  
+  ols_model2 <- lm(y ~ x2, data = df_iter)
+  sim_results$beta_hat_x2[i] <- coef(ols_model2)[["x2"]]
+  
+  ols_model3 <- lm(y ~ x3, data = df_iter)
+  sim_results$beta_hat_x3[i] <- coef(ols_model3)[["x3"]]
+}
+
+# LLN to estimate plim
+mean(sim_results$beta_hat_x1)
+mean(sim_results$beta_hat_x2)
+mean(sim_results$beta_hat_x3)
+
+
